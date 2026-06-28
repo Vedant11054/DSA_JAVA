3854e0802da4db4eedee864a2f2a0a45c996d345
Vedant11054 <164635371+Vedant11054@users.noreply.github.com>
2026-06-28 18:24:45 +0530

    Time: 14 ms (70.36%), Space: 47.7 MB (31.55%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -7,34 +7,39 @@ class Solution {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        class Pair {
-            int first;   // frequency
-            int second;  // number
+        class pair {
+            int first;   
+            int second;  
 
-            Pair(int f, int s) {
+            pair(int f, int s) {
                 first = f;
                 second = s;
             }
         }
 
-        PriorityQueue<Pair> heap = new PriorityQueue<>(
+        PriorityQueue<pair> heap = new PriorityQueue<>(
             (a, b) -> Integer.compare(a.first, b.first)
         );
 
-        for (int key : map.keySet()) {
-            heap.offer(new Pair(map.get(key), key));
 
-            if (heap.size() > k) {
+        for (int key : map.keySet())
+        {
+            pair curr=new pair(map.get(key),key);
+            if(heap.size()<k)
+            {
+                heap.add(curr);
+            }
+            else if (curr.first > heap.peek().first)
+            {
                 heap.poll();
+                heap.add(curr);
             }
         }
-
-        int[] ans = new int[k];
-
-        for (int i = k - 1; i >= 0; i--) {
-            ans[i] = heap.poll().second;
+        int[] ans=new int[k];
+        for(int i=k-1;i>=0;i--)
+        {
+            ans[i]=heap.poll().second;
         }
-
         return ans;
     }
 }
