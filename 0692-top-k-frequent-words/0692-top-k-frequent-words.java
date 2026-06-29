0450d48451faa187bfdd550a37249b5c08c0ce8e
Vedant11054 <164635371+Vedant11054@users.noreply.github.com>
2026-06-30 02:27:57 +0530

    Time: 7 ms (85.88%), Space: 46.1 MB (97.67%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,37 +1,51 @@
 class Solution {
-    public List<String> topKFrequent(String[] words, int k) {
-
-        HashMap<String, Integer> map = new HashMap<>();
-
-        for (String word : words) {
-            map.put(word, map.getOrDefault(word, 0) + 1);
+    public List<String> topKFrequent(String[] words, int k) 
+    {
+        HashMap<String,Integer>map=new HashMap<>();
+        for(int i=0;i<words.length;i++)
+        {
+            map.put(words[i],map.getOrDefault(words[i],0)+1);
         }
-
-        PriorityQueue<String> heap = new PriorityQueue<>(
-            (a, b) -> {
-                if (!map.get(a).equals(map.get(b))) {
-                    return map.get(a) - map.get(b); 
+        class pair
+        {
+            int first;
+            String second;
+            pair(int f,String s)
+            {
+                first=f;
+                second=s;
+            }
+        }
+        PriorityQueue<pair>heap=new PriorityQueue<>(
+            (a,b)->
+            {
+                if(a.first!=b.first)
+                {
+                    return a.first-b.first;
                 }
-                return b.compareTo(a); 
+                return b.second.compareTo(a.second);
             }
         );
-
-        for (String word : map.keySet()) {
-            heap.offer(word);
-
-            if (heap.size() > k) {
+        for(String key:map.keySet())
+        {
+            pair curr=new pair(map.get(key),key);
+            if(heap.size()<k)
+            {
+                heap.add(curr);
+            }
+            else if (curr.first > heap.peek().first || (curr.first == heap.peek().first && curr.second.compareTo(heap.peek().second) < 0))
+            {
                 heap.poll();
+                heap.add(curr);
             }
         }
-
-        List<String> ans = new ArrayList<>();
-
-        while (!heap.isEmpty()) {
-            ans.add(heap.poll());
+        List<String>ans=new ArrayList<>();
+        while(heap.size()>0)
+        {
+            ans.add(heap.poll().second);
         }
-
         Collections.reverse(ans);
-
         return ans;
+
     }
 }
