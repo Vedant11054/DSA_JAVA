7a8ce8e2f10724dedba96f0a81d1f841c08e4e01
Vedant11054 <164635371+Vedant11054@users.noreply.github.com>
2026-06-30 00:59:25 +0530

    Time: 14 ms (70.36%), Space: 47.5 MB (65.45%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,41 +1,45 @@
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+    public int[] topKFrequent(int[] nums, int k) 
+    {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+           map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
-
-        class pair {
-            int first;   
-            int second;  
-
-            pair(int f, int s) {
-                first = f;
-                second = s;
+        class pair
+        {
+            int first;
+            int second;
+            pair(int f, int s)
+            {
+                first =f;
+                second =s;
             }
         }
-
-        PriorityQueue<pair> heap = new PriorityQueue<>(
-            (a, b) -> Integer.compare(a.first, b.first)
+        PriorityQueue<pair>heap=new PriorityQueue<>(
+            (a,b)->
+            {
+                if(a.first!=b.first)
+                {
+                    return a.first-b.first;
+                }
+                return a.second-b.second;
+            }
         );
-
-
-        for (int key : map.keySet())
+        for(int key:map.keySet())
         {
             pair curr=new pair(map.get(key),key);
             if(heap.size()<k)
             {
                 heap.add(curr);
             }
-            else if (curr.first > heap.peek().first)
+            else if(curr.first>heap.peek().first)
             {
                 heap.poll();
                 heap.add(curr);
             }
         }
-        int[] ans=new int[k];
+        int ans[]=new int[k];
         for(int i=k-1;i>=0;i--)
         {
             ans[i]=heap.poll().second;
